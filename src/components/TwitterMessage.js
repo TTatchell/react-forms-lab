9362006e0b80f257944b9bad3fa3027e65550fe0
TTatchell <tim.tatchell24@gmail.com>
2021-05-26 21:47:11 +1000

    up[load

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,38 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
+  constructor(props) {
     super();
 
-    this.state = {};
+    this.state = {
+      maxChars: props.maxChars,
+      message: ''
+
+    };
+  }
+
+  handleChange = (event) => {
+    console.log(event.target.value)
+    this.setState = ({
+      message: event.target.value
+    })
+    console.log(this.state.message)
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+
+        <input
+          type="text"
+          name="message"
+          id="message"
+          onChange={event => this.handleChange(event)}
+          
+          />
+          
+
       </div>
     );
   }
